56e10cac2fd5ede7835a5f2ff62fb30de20289a3
cyber-sparky <mdpranaw@gmail.com>
2023-02-23 16:18:40 +0530

    updated profile and login
    updated course in course page and added profile page, added logic in login page, add mobile hacking intro

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,15 @@
-document.querySelector("#learnBtn").addEventListener("click",function(){
-    window.location.href="./course/course.html";
-})
-document.querySelector("#profile").addEventListener("click",function(){
-    window.location.href="./login/signin.html";
-})
+// document.querySelector("#learnBtn").addEventListener("click",function(){
+//     window.location.href="./course/course.html";
+// })
+// document.querySelector("#profile").addEventListener("click",function(){
+//     window.location.href="./login/signin.html";
+// })
 
-document.querySelector("form").addEventListener("subvmit",function(e){
+// document.querySelector("form").addEventListener("submit",function(e){
+//     e.preventDefault();
+// })
+
+document.querySelector("#dontHaveAccount").addEventListener("click",function(e){
     e.preventDefault();
+    window.location.href="./signup.html";
 })
