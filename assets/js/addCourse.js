53d9f546b134933874f93cee0915473e0b9a40c6
cyber-sparky <mdpranaw@gmail.com>
2023-04-27 01:31:24 +0530

    added course to profile page

---
+++ assets/js/addCourse.js
@@ -0,0 +1,49 @@
+let enroll = document.getElementById("enroll");
+let arr = JSON.parse(localStorage.getItem("course_list"));
+let user = JSON.parse(localStorage.getItem("profile"));
+let res;
+
+enroll.addEventListener("click", (e) => {
+  let user = JSON.parse(localStorage.getItem("profile"));
+  let course_id = e.target.dataset.id;
+  let enrolled_data = {
+    user_name: user.email,
+    enrolled_course: course_id,
+    is_enrolled: true,
+  };
+  // console.log(enrolled_data);
+  arr = localStorage.getItem("course_list")
+    ? JSON.parse(localStorage.getItem("course_list"))
+    : [];
+  // console.log(arr);
+  arr.push(enrolled_data);
+
+  localStorage.setItem("course_list", JSON.stringify(arr));
+  window.location.href = "./webhacking.html"   
+});
+
+// arr.find((el) => {
+//   if (el["enrolled_course"] == enroll.dataset.id && el["user_name"] == user.email && el["is_enrolled"] == true) {
+//     // checkbox[0].setAttribute("checked", "");
+//     // e.target.parentElement.parentElement.setAttribute("href","#")
+
+//     // enroll.setAttribute("disabled","")
+//     enroll.style.display="";
+//     // enroll.style.backgroundColor="black"
+//       // e.target.style.display="none"
+// // return res=1;
+
+// }
+// // else{
+// //   //./stagesOfHacking.html
+// //   e.target.parentElement.parentElement.setAttribute("href","./stagesOfHacking.html")
+// // }
+// });
+
+// arr.find((el) => {
+//   if (el["enrolled_course"] == enroll.dataset.id && el["user_name"] == user.email) {
+//     // checkbox[0].setAttribute("checked", "");
+//     // checkbox[0].setAttribute("disabled", "")
+//     alert("already enrolled");
+//   }
+// });
--- assets/js/course.js
+++ assets/js/course.js
@@ -1,3 +1,5 @@
+JSON.parse(localStorage.getItem("course_list")) 
+JSON.parse(localStorage.getItem("profile"));
 // course page
 const cards = [
   {
@@ -5,54 +7,63 @@ const cards = [
     title: "Things you should know",
     desc: "Basic things that you should know about hacking before getting into this",
     redirection: "stages",
+    dataset: "thingsYouShouldKnow"
   },
   {
     imgSource: "../assets/img/web.jpg",
     title: "Web Hacking",
     desc: "Things which are all done by hackers by web",
     redirection: "webhack",
+    dataset:"web"
   },
   {
     imgSource: "../assets/img/mobileHacking.jpg",
     title: "Hacking through mobile",
-    desc: "Things Which hackers do with phone",
+    desc: "You will learn all about ethical mobile hacking on Android in this course! Course is strictly for ethical hacking and educational purposes!",
     redirection: "mobilehack",
+    dataset:"mobilehack"
   },
   {
     imgSource: "../assets/img/scripting.jpg",
     title: "Scripting",
-    desc: "a programming language that employs a high-level construct to interpret and execute one command at a time",
+    desc: "A programming language that employs a high-level construct to interpret and execute one command at a time",
     redirection: "script",
+    dataset:"script"
   },
   {
     imgSource: "../assets/img/crossSite.jpg",
     title: "Cross-site script inclusion",
     desc: "XSS attacks are a type of injection, in which malicious scripts are injected into otherwise benign and trusted websites",
-    redirection: "",
+    redirection: "stages",
+    dataset:"xxs"
   },
   {
     imgSource: "../assets/img/linux.jpg",
     title: "Kali Linux Course",
     desc: "Kali Linux is not about its tools, nor the operating system. Kali Linux is a platform.",
-    redirection: "",
+    redirection: "stages",
+    dataset:"kali"
   },
   {
     imgSource: "../assets/img/humanHack.jpg",
     title: "Human Hacking",
     desc: "A global security expert draws on psychological insights to help you master the art of social engineering",
     redirection: "",
+    dataset:"humanhack"
   },
   {
     imgSource: "../assets/img/malware.jpg",
     title: "Malverting",
     desc: "a relatively new cyberattack technique that injects malicious code within digital ads",
     redirection: "",
+    dataset:"malware"
   },
   {
     imgSource: "../assets/img/network.jpg",
     title: "Network Security",
     desc: "Network security is any activity designed to protect the usability and integrity of your network and data.",
     redirection: "",
+    dataset:"network"
   },
   
 ];
@@ -83,20 +94,58 @@ for (let i = cards.length - 1; i >= 0; i--) {
   startLearn.innerText = "Start Learning";
   startLearn.setAttribute("id", cards[i]["redirection"]);
   startLearn.setAttribute("class", "learnBtn");
+  startLearn.setAttribute("data-id", cards[i]["dataset"]);
   document.querySelector("div.container").append(startLearn);
 }
-document.querySelector("#stages").addEventListener("click", function () {
-  window.location.href = "../learning_pages/thingsShouldKnowIntro.html";
+let stage=document.querySelector("#stages");
+stage.addEventListener("click", function () {
+  
+  // arr.find((el) => {
+  //   if (el["enrolled_course"] == stage.dataset.id && el["user_name"] == user.email && el["is_enrolled"] == true) {
+  //     window.location.href = "../learning_pages/stagesOfHacking.html";
+  // } 
+  // else{
+    window.location.href = "../learning_pages/thingsShouldKnowIntro.html";
+  // }
+  // }) 
+ 
 });
-document.querySelector("#webhack").addEventListener("click", function () {
-  window.location.href = "../learning_pages/webhackingintro.html";
-});
-document.querySelector("#mobilehack").addEventListener("click", function () {
-  window.location.href = "../learning_pages/mobileHackingIntro.html";
-});
-document.querySelector("#script").addEventListener("click", function () {
-  window.location.href = "../learning_pages/scripting.html";
-});
+// let webhack=document.querySelector("#webhack")
+// webhack.addEventListener("click", function () {
+//   // window.location.href = "../learning_pages/webhackingintro.html";
+//   arr.find((el) => {
+//     if (el["enrolled_course"] == webhack.dataset.id && el["user_name"] == user.email && el["is_enrolled"] == true) {
+//       window.location.href = "../learning_pages/webhacking.html";
+//   } 
+//   else{
+//     window.location.href = "../learning_pages/webhackingintro.html";
+//   }
+//   });
+// });
+// let mobilehack=document.querySelector("#mobilehack")
+// mobilehack.addEventListener("click", function () {
+//   arr.find((el) => {
+//     if (el["enrolled_course"] == mobilehack.dataset.id && el["user_name"] == user.email && el["is_enrolled"] == true) {
+//       window.location.href = "../learning_pages/mobileHackingIntro.html";
+//   } 
+//   else{
+//     window.location.href = "../learning_pages/thingsShouldKnowIntro.html";
+//   }
+//   });
+//   // window.location.href = "../learning_pages/mobileHackingIntro.html";
+// });
+// let script=document.querySelector("#script")
+// script.addEventListener("click", function () {
+//   arr.find((el) => {
+//     if (el["enrolled_course"] == script.dataset.id && el["user_name"] == user.email && el["is_enrolled"] == true) {
+//       window.location.href = "../learning_pages/scripting.html";
+//   } 
+//   else{
+//     window.location.href = "../learning_pages/thingsShouldKnowIntro.html";
+//   }
+//   });
+//   // window.location.href = "../learning_pages/scripting.html";
+// });
 
 
 let search=document.getElementById("searchCourse");
@@ -113,3 +162,49 @@ let search=document.getElementById("searchCourse");
                 }
             });
         })
+
+let course_list = JSON.parse(localStorage.getItem("course_list"))
+
+let learnBtn = document.querySelectorAll(".learnBtn")
+// console.log(learnBtn[1]);
+
+learnBtn[0].addEventListener("click",function(e){
+  console.log(e.target.dataset.id);
+  course_list.find(el=>{
+    if(el["enrolled_course"]==e.target.dataset.id){
+      window.location.href="../learning_pages/stagesOfHacking.html"
+    }else{
+      window.location.href="../learning_pages/thingsShouldKnowIntro.html"
+    }
+  })
+})
+learnBtn[1].addEventListener("click",function(e){
+  console.log(e.target.dataset.id);
+  course_list.find(el=>{
+    if(el["enrolled_course"]==e.target.dataset.id){
+      window.location.href="../learning_pages/webhacking.html"
+    }else{
+      window.location.href="../learning_pages/webhackingintro.html"
+    }
+  })
+})
+learnBtn[2].addEventListener("click",function(e){
+  console.log(e.target.dataset.id);
+  course_list.find(el=>{
+    if(el["enrolled_course"]==e.target.dataset.id){
+      window.location.href="../learning_pages/mobileHacking.html"
+    }else{
+      window.location.href="../learning_pages/mobileHackingIntro.html"
+    }
+  })
+})
+learnBtn[3].addEventListener("click",function(e){
+  console.log(e.target.dataset.id);
+  course_list.find(el=>{
+    if(el["enrolled_course"]==e.target.dataset.id){
+      window.location.href="../learning_pages/scripting.html"
+    }else{
+      window.location.href="../learning_pages/scriptingIntro.html"
+    }
+  })
+})
